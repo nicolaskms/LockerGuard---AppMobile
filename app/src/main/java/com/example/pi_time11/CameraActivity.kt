0130fcb4451abaf1fa95230d12cb9f4af4460ddd
Henrique Monteiro <80502511+BarryHook1@users.noreply.github.com>
2024-05-16 19:26:13 -0300

    Alteração do local salvo da foto
    Salvar a foto na galeria do usuario.

--- app/src/main/java/com/example/pi_time11/CameraActivity.kt
+++ app/src/main/java/com/example/pi_time11/CameraActivity.kt
@@ -1,7 +1,11 @@
 package com.example.pi_time11
 
+import android.content.ContentValues
+import android.icu.text.SimpleDateFormat
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -13,6 +17,7 @@ import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.core.content.ContextCompat
 import java.io.File
+import java.util.Locale
 
 class CameraActivity : AppCompatActivity() {
 
@@ -25,48 +30,54 @@ class CameraActivity : AppCompatActivity() {
 
         cameraView = findViewById(R.id.camera_view)
 
-        // Configurar a câmera
         setupCamera()
 
-        // Configurar o botão de captura de foto
         val captureButton: Button = findViewById(R.id.capture_button)
-        captureButton.setOnClickListener {
-            capturePhoto()
-        }
+        captureButton.setOnClickListener { capturePhoto() }
     }
+
     private fun setupCamera() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
         cameraProviderFuture.addListener({
             val cameraProvider = cameraProviderFuture.get()
 
-            // Configurar Preview
             val preview = Preview.Builder().build()
             preview.setSurfaceProvider(cameraView.surfaceProvider)
 
-            // Configurar ImageCapture
             imageCapture = ImageCapture.Builder().build()
 
-            // Ligar a câmera ao ciclo de vida
             cameraProvider.bindToLifecycle(this, CameraSelector.DEFAULT_BACK_CAMERA, preview, imageCapture)
         }, ContextCompat.getMainExecutor(this))
     }
 
     private fun capturePhoto() {
-        val file = File(externalMediaDirs.first(), "${System.currentTimeMillis()}.jpg")
+        val name = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(System.currentTimeMillis())
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                put(MediaStore.MediaColumns.RELATIVE_PATH, "DCIM/CameraX-Photos")
+            }
+        }
 
-        val outputOptions = ImageCapture.OutputFileOptions.Builder(file).build()
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(
+            contentResolver,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            contentValues
+        ).build()
 
         imageCapture.takePicture(outputOptions, ContextCompat.getMainExecutor(this),
             object : ImageCapture.OnImageSavedCallback {
                 override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
-                    // Foto salva com sucesso
-                    val savedUri = Uri.fromFile(file)
-                    Toast.makeText(this@CameraActivity, "Foto salva em: $savedUri", Toast.LENGTH_LONG).show()
+                    val savedUri = outputFileResults.savedUri
+                    val msg = "Foto salva em: $savedUri"
+                    Toast.makeText(this@CameraActivity, msg, Toast.LENGTH_SHORT).show()
+
+                    ///TODO Redirecionar para outra tela após tirar a foto
                 }
 
                 override fun onError(exception: ImageCaptureException) {
-                    // Erro ao salvar a foto
-                    Toast.makeText(this@CameraActivity, "Erro ao salvar a foto", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this@CameraActivity, "Erro ao salvar a foto: ${exception.message}", Toast.LENGTH_SHORT).show()
                 }
             })
     }
