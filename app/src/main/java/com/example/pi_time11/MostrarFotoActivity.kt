c2f170354e1d8a5bb0565fa2cb154842ee711857
Henrique Monteiro <80502511+BarryHook1@users.noreply.github.com>
2024-05-24 12:53:59 -0300

    Ajustes do layout

--- app/src/main/java/com/example/pi_time11/MostrarFotoActivity.kt
+++ app/src/main/java/com/example/pi_time11/MostrarFotoActivity.kt
@@ -1,5 +1,6 @@
 package com.example.pi_time11
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
@@ -11,15 +12,35 @@ import androidx.core.view.WindowInsetsCompat
 
 class MostrarFotoActivity : AppCompatActivity() {
     private lateinit var imageView: ImageView
+    private lateinit var buttonVoltar: Button
+    private lateinit var btnContinuar: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mostrar_foto)
+
+        buttonVoltar = findViewById(R.id.btnVoltar_camera)
+        btnContinuar = findViewById(R.id.btnContinuar_Camera)
 
         imageView = findViewById(R.id.image_views)
         val photoUri: String? = intent.getStringExtra("photoUri")
 
         photoUri?.let {
             imageView.setImageURI(Uri.parse(it))
+        }
+
+        buttonVoltar.setOnClickListener{
+            val intent = Intent(this, CameraActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        btnContinuar.setOnClickListener {
+            // Iniciar a atividade de pagamento (CartaoActivity)
+            ///TODO Mandar para NFC
+           // val intent = Intent(this, NFC::class.java)
+           // startActivity(intent)
+           // finish()
         }
     }
 }
