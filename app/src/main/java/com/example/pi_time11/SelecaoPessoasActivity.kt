d84360dc938721dd1a33a76d37c636187e401ae8
Henrique Monteiro <80502511+BarryHook1@users.noreply.github.com>
2024-05-24 12:30:18 -0300

    ....

--- app/src/main/java/com/example/pi_time11/SelecaoPessoasActivity.kt
+++ app/src/main/java/com/example/pi_time11/SelecaoPessoasActivity.kt
@@ -18,27 +18,24 @@ class SelecaoPessoasActivity : AppCompatActivity() {
 
         val buttonConfirmar = findViewById<Button>(R.id.buttonConfirmar)
         buttonConfirmar.setOnClickListener {
-
             val radioGroup = findViewById<RadioGroup>(R.id.radioGroup)
             val selectedRadioButtonId = radioGroup.checkedRadioButtonId
             if (selectedRadioButtonId != -1) {
-
                 val selectedOption = when (radioGroup.checkedRadioButtonId) {
                     R.id.rUmaPessoa -> 1
                     R.id.rDuasPessoas -> 2
                     else -> 1
                 }
-
+                // Passar o número de pessoas selecionadas
+                val intent = Intent(this, CameraActivity::class.java)
                 intent.putExtra("numPessoas", selectedOption)
-                irParaCamera()
-
+                startActivity(intent)
             } else {
                 Toast.makeText(this, "Por favor, selecione uma opção", Toast.LENGTH_SHORT).show()
             }
         }
     }
-
-    private fun irParaCamera(){
+    fun irParaCamera(){
         val intent = Intent(this, CameraActivity::class.java)
         startActivity(intent)
         finish()
