24c84dbc973268cca0bcc4de851f078861d19fcf
Henrique Monteiro <80502511+BarryHook1@users.noreply.github.com>
2024-05-24 15:01:45 -0300

    TESTE VER FOTO SEM NFC (TEST)

--- app/src/main/java/com/example/pi_time11/FotoActivity.kt
+++ app/src/main/java/com/example/pi_time11/FotoActivity.kt
@@ -1,20 +1,44 @@
 package com.example.pi_time11
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class FotoActivity : AppCompatActivity() {
+    private lateinit var imageView: ImageView
+    private lateinit var buttonVoltar: Button
+    private lateinit var btnContinuar: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_foto)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        buttonVoltar = findViewById(R.id.back_button)
+        btnContinuar = findViewById(R.id.continue_button)
+
+        imageView = findViewById(R.id.image_view)
+        val photoUri: String? = intent.getStringExtra("photoUri")
+
+        photoUri?.let {
+            imageView.setImageURI(Uri.parse(it))
+        }
+
+        buttonVoltar.setOnClickListener{
+            val intent = Intent(this, CameraActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        btnContinuar.setOnClickListener {
+             val intent = Intent(this, LiberarLocActivity::class.java)
+             startActivity(intent)
+             finish()
         }
     }
 }
--- app/src/main/java/com/example/pi_time11/GerenteActivity.kt
+++ app/src/main/java/com/example/pi_time11/GerenteActivity.kt
@@ -11,6 +11,7 @@ class GerenteActivity : AppCompatActivity() {
     private lateinit var btnLocarArmario: AppCompatImageButton
     private lateinit var btnLiberarLoc: AppCompatImageButton
     private lateinit var buttonSair: Button
+    private lateinit var buttonFoto: Button
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,15 @@ class GerenteActivity : AppCompatActivity() {
         btnLocarArmario = findViewById(R.id.btnLocarArmario)
         btnLiberarLoc = findViewById(R.id.btnLiberarLoc)
         buttonSair = findViewById(R.id.buttonSair)
+        buttonFoto = findViewById(R.id.button_ver_foto)
         auth = FirebaseAuth.getInstance()
 
+
+        buttonSair.setOnClickListener {
+            val intent = Intent(this, FotoActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
 
         btnLiberarLoc.setOnClickListener {
             val intent = Intent(this, LiberarLocFirstActivity::class.java)
